6d5143bfd7b477c528e27cc55ca33723688ad70c
Alec <alecmichaelmiller@gmail.com>
2021-10-30 19:29:36 -0700

    did stuff

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 export enum Game {
     VRC_TIPPING_POINT
 }
+
+export enum AgeGroup {
+    MIDDLE_SCHOOL,
+    HIGH_SCHOOL
+}
+
+export interface Team {
+    id?: number;
+    number: string;
+    name: string;
+    city: string;
+    region: string;
+    country: string;
+    shortName: string;
+    school: string;
+    sponsors: string;
+    tiebreaker: number;
+    divisionId: number;
+    checkedIn: boolean;
+    ageGroup: AgeGroup;
+}
